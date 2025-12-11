fd42ff5357f65a90415ab1397f96fedd37060cc2
Tina2510 <tinachinivar@gmail.com>
2025-12-11 14:44:15 +0530

    computing employee wage foe multiple companies

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -1,12 +1,9 @@
 import java.util.Random;
+
 public class EmpWageBuilder {
+
     private final int FULL_TIME = 1;
     private final int PART_TIME = 2;
-    private final int WAGE_PER_HOUR = 20;
-    private final int FULL_DAY_HOUR = 8;
-    private final int PART_TIME_HOUR = 8;
-    private final int MAX_DAYS = 20;
-    private final int MAX_HOURS = 100;
 
     private Random rand;
 
@@ -14,70 +11,43 @@ public class EmpWageBuilder {
     public EmpWageBuilder() {
         rand = new Random();
     }
-    public int computeDailyWage() {
-        int empCheck = rand.nextInt(2); // 0 or 1
-        int wage = (empCheck == 1) ? FULL_DAY_HOUR * WAGE_PER_HOUR : 0;
-        System.out.println("Daily Wage = " + wage);
-        return wage;
-    }
 
-
-    public int computePartTimeWage() {
-        int empCheck = rand.nextInt(3); // 0,1,2
-        int hours = (empCheck == FULL_TIME) ? FULL_DAY_HOUR :
-                (empCheck == PART_TIME) ? PART_TIME_HOUR : 0;
-        int wage = hours * WAGE_PER_HOUR;
-        System.out.println("Part-Time/Full-Time Wage = " + wage);
-        return wage;
-    }
-    public int computeWageUsingSwitch() {
-        int empCheck = rand.nextInt(3);
-        int hours = 0;
-
-        switch(empCheck) {
-            case FULL_TIME: hours = FULL_DAY_HOUR; break;
-            case PART_TIME: hours = PART_TIME_HOUR; break;
-            default: hours = 0;
+    private int getWorkingHours(int empCheck, int fullDayHour, int partTimeHour) {
+        switch (empCheck) {
+            case FULL_TIME:
+                return fullDayHour;
+            case PART_TIME:
+                return partTimeHour;
+            default:
+                return 0;
         }
-
-        int wage = hours * WAGE_PER_HOUR;
-        System.out.println("Wage using Switch = " + wage);
-        return wage;
     }
 
-    public int computeWageForMonth() {
+    public void computeWageForCompany(String company, int wagePerHour, int maxWorkingDays, int maxWorkingHours) {
+
+        int totalHours = 0;
+        int totalDays = 0;
         int totalWage = 0;
-        for(int day=1; day<=MAX_DAYS; day++){
-            totalWage += computeWageUsingSwitch();
-        }
-        System.out.println("Total Wage for Month = " + totalWage);
-        return totalWage;
-    }
 
-    public int computeWageTillCondition() {
-        int totalHours = 0, totalDays = 0, totalWage = 0;
-        while(totalHours < MAX_HOURS && totalDays < MAX_DAYS){
+        while (totalHours < maxWorkingHours && totalDays < maxWorkingDays) {
+
             int empCheck = rand.nextInt(3);
-            int hours = 0;
-            switch(empCheck){
-                case FULL_TIME: hours = FULL_DAY_HOUR; break;
-                case PART_TIME: hours = PART_TIME_HOUR; break;
-                default: hours = 0;
-            }
-            totalHours += hours;
-            totalWage += hours * WAGE_PER_HOUR;
+            int hoursWorked = getWorkingHours(empCheck, 8, 8);
+            totalHours += hoursWorked;
+            totalWage += hoursWorked * wagePerHour;
             totalDays++;
         }
-        System.out.println(" Total Wage till Condition = " + totalWage);
-        return totalWage;
+
+        System.out.println("Company: " + company + "  Total Wage = " + totalWage);
     }
 
-    public static void main(String[] args){
-        EmpWageBuilder emp = new EmpWageBuilder();
-        emp.computeDailyWage();
-        emp.computePartTimeWage();
-        emp.computeWageUsingSwitch();
-        emp.computeWageForMonth();
-        emp.computeWageTillCondition();
+
+    public static void main(String[] args) {
+        EmpWageBuilder empWage = new EmpWageBuilder();
+
+
+        empWage.computeWageForCompany("TCS", 20, 20, 100);
+        empWage.computeWageForCompany("Infosys", 25, 22, 120);
+        empWage.computeWageForCompany("Wipro", 30, 18, 90);
     }
 }
