5c9c400fd3dfbeb60f68e7fe4a34e4729e807d71
Tina2510 <tinachinivar@gmail.com>
2025-12-11 15:06:09 +0530

    total wage

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -5,49 +5,58 @@ public class EmpWageBuilder {
     private final int FULL_TIME = 1;
     private final int PART_TIME = 2;
 
+    private CompanyEmpWage companyEmpWage;
     private Random rand;
 
 
-    public EmpWageBuilder() {
-        rand = new Random();
+    public EmpWageBuilder(CompanyEmpWage companyEmpWage) {
+        this.companyEmpWage = companyEmpWage;
+        this.rand = new Random();
     }
 
-    private int getWorkingHours(int empCheck, int fullDayHour, int partTimeHour) {
+
+    private int getWorkingHours(int empCheck) {
         switch (empCheck) {
-            case FULL_TIME:
-                return fullDayHour;
-            case PART_TIME:
-                return partTimeHour;
-            default:
-                return 0;
+            case FULL_TIME: return 8;
+            case PART_TIME: return 8;
+            default: return 0;
         }
     }
 
-    public void computeWageForCompany(String company, int wagePerHour, int maxWorkingDays, int maxWorkingHours) {
 
+    public void computeWage() {
         int totalHours = 0;
         int totalDays = 0;
         int totalWage = 0;
 
-        while (totalHours < maxWorkingHours && totalDays < maxWorkingDays) {
-
+        while (totalHours < companyEmpWage.maxHours && totalDays < companyEmpWage.workingDays) {
             int empCheck = rand.nextInt(3);
-            int hoursWorked = getWorkingHours(empCheck, 8, 8);
+            int hoursWorked = getWorkingHours(empCheck);
             totalHours += hoursWorked;
-            totalWage += hoursWorked * wagePerHour;
+            totalWage += hoursWorked * companyEmpWage.wagePerHour;
             totalDays++;
         }
 
-        System.out.println("Company: " + company + "  Total Wage = " + totalWage);
+
+        companyEmpWage.setTotalWage(totalWage);
+        System.out.println(companyEmpWage);
     }
 
 
     public static void main(String[] args) {
-        EmpWageBuilder empWage = new EmpWageBuilder();
+
+        CompanyEmpWage tcs = new CompanyEmpWage("TCS", 20, 20, 100);
+        CompanyEmpWage infosys = new CompanyEmpWage("Infosys", 25, 22, 120);
+        CompanyEmpWage wipro = new CompanyEmpWage("Wipro", 30, 18, 90);
 
 
-        empWage.computeWageForCompany("TCS", 20, 20, 100);
-        empWage.computeWageForCompany("Infosys", 25, 22, 120);
-        empWage.computeWageForCompany("Wipro", 30, 18, 90);
+        EmpWageBuilder empTCS = new EmpWageBuilder(tcs);
+        EmpWageBuilder empInfosys = new EmpWageBuilder(infosys);
+        EmpWageBuilder empWipro = new EmpWageBuilder(wipro);
+
+
+        empTCS.computeWage();
+        empInfosys.computeWage();
+        empWipro.computeWage();
     }
 }
