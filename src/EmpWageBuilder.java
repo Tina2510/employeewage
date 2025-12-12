b6fc42c06cdbda05c89d070463ede06cba51b1f6
Tina2510 <tinachinivar@gmail.com>
2025-12-12 11:10:15 +0530

    updated

--- src/EmpWageBuilder.java
+++ src/EmpWageBuilder.java
@@ -1,6 +1,4 @@
-
 import java.util.ArrayList;
-
 import java.util.HashMap;
 
 public class EmpWageBuilder implements IComputeEmpWage {
@@ -8,72 +6,26 @@ public class EmpWageBuilder implements IComputeEmpWage {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
 
-
     private ArrayList<CompanyEmpWage> companyEmpWageList;
-    private HashMap<String, Integer> companyToTotalWageMap;
-
-
-
-    public EmpWageBuilder() {
-        companyEmpWageList = new ArrayList<>();
-        companyToTotalWageMap = new HashMap<>();
-
     private HashMap<String, CompanyEmpWage> companyMap;
 
     public EmpWageBuilder() {
+        companyEmpWageList = new ArrayList<>();
         companyMap = new HashMap<>();
-
     }
 
     @Override
     public void addCompany(String name, int wagePerHour, int workingDays, int maxHours) {
         CompanyEmpWage company = new CompanyEmpWage(name, wagePerHour, workingDays, maxHours);
-
         companyEmpWageList.add(company);
-
         companyMap.put(name, company);
-
     }
 
     @Override
     public void computeWages() {
-
         for (CompanyEmpWage company : companyEmpWageList) {
             int totalWage = computeEmpWage(company);
             company.setTotalWage(totalWage);
-            companyToTotalWageMap.put(company.company, totalWage);
-        }
-    }
-
-    @Override
-    public int getTotalWage(String companyName) {
-        return companyToTotalWageMap.getOrDefault(companyName, 0);
-    }
-
-    private int computeEmpWage(CompanyEmpWage company) {
-        int totalHours = 0;
-        int totalDays = 0;
-
-        while (totalHours <= company.maxHours && totalDays < company.workingDays) {
-            totalDays++;
-            int empHrs = getEmpHours();
-            totalHours += empHrs;
-        }
-
-        return totalHours * company.empRatePerHour;
-    }
-
-    private int getEmpHours() {
-        int empCheck = (int) (Math.random() * 10) % 3;
-        switch (empCheck) {
-            case IS_FULL_TIME: return 8;
-            case IS_PART_TIME: return 4;
-            default: return 0;
-        }
-
-        for (CompanyEmpWage company : companyMap.values()) {
-            int totalWage = computeEmpWage(company);
-            company.setTotalWage(totalWage);
         }
     }
 
@@ -83,12 +35,10 @@ public class EmpWageBuilder implements IComputeEmpWage {
 
         while (totalHours <= company.maxHours && totalDays < company.workingDays) {
             totalDays++;
-            int empHrs = getEmpHours();
-            totalHours += empHrs;
+            totalHours += getEmpHours();
         }
 
         return totalHours * company.empRatePerHour;
-
     }
 
     private int getEmpHours() {
@@ -105,17 +55,9 @@ public class EmpWageBuilder implements IComputeEmpWage {
         CompanyEmpWage company = companyMap.get(companyName);
         return company != null ? company.totalWage : 0;
     }
-    public class Main {
-        public static void main(String[] args) {
-            EmpWageBuilder empBuilder = new EmpWageBuilder();
-            empBuilder.addCompany("ABC", 20, 20, 100);
-            empBuilder.addCompany("XYZ", 25, 22, 120);
 
-            empBuilder.computeWages();
-
-            System.out.println("Total wage for ABC: " + empBuilder.getTotalWage("ABC"));
-            System.out.println("Total wage for XYZ: " + empBuilder.getTotalWage("XYZ"));
-        }
+    // Getter for ArrayList (for UC12 usage)
+    public ArrayList<CompanyEmpWage> getCompanyList() {
+        return companyEmpWageList;
     }
-
 }
